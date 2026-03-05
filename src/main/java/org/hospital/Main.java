82ce5979adb932b75958b1cbb4a74945ef1e00ca
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-03-05 18:13:13 +0100

    fix changed wrong data

--- src/main/java/org/hospital/Main.java
+++ src/main/java/org/hospital/Main.java
@@ -8,10 +8,10 @@ import org.hospital.Student;
 public class Main {
     static void main() {
         Character alberto = new AppoinmentPatient ("Alfredo Diaz", "2026-03-01", 101,"Rest and Water","2026-03-05", "2026-03-05", "Nurse Joy");
-        Character javier = new  AppoinmentPatient ("Javier Longueira", "2026-03-02", 105, "2026-03-04", "Vitamin B12", "Follow-up", "Nurse Ratched");
+        Character javier = new  AppoinmentPatient ("Javier Longueira", "2026-03-02", 105, "Vitamin B12", "2026-03-04","Follow-up", "Nurse Ratched");
 
-        Character david = new AmbulancePatient ("David Alvarez", "2026-03-03", 304, "2026-03-20", "Antibiotics", "Leg Surgery", "Physical Therapy");
-        Character daniel = new AmbulancePatient ("Daniel Vallejo", "2026-03-03", 206, "Pending", "Painkillers", "Shoulder Surgery", "Restricted Movement");
+        Character david = new AmbulancePatient ("David Alvarez", "2026-03-03", 304, "Antibiotics", "2026-03-20", "2026-03-27", "Physical Therapy");
+        Character daniel = new AmbulancePatient ("Daniel Vallejo", "2026-03-03", 206, "Painkillers", "Pending","2026-03-12", "Restricted Movement");
 
         Character dorian = new Specialist("Dorian Gray", "14:00 - 15:00", "Day Shift", "30", "Diagnostic Medicine");
         Character vlad = new Specialist("Vlad Tepes", "13:00 - 14:00", "Night Shift", "25", "General Surgery");
